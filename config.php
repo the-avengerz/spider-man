fd5f5f21ea1832c65de240aaec5abd670a066190
JanHuang <bboyjanhuang@gmail.com>
2017-01-15 12:32:27 +0800

    add weibo pipeline and add faker

--- config.php
+++ config.php
@@ -40,5 +40,25 @@ return [
             ],
         ],
         'download_path' => __DIR__ . '/downloads/weibo'
+    ],
+    'weibo_hz' => [
+        'index' => new \weibo_hz\WeiBoPipeline('GET', 'http://m.weibo.cn/container/getIndex?type=uid&value=6049220524&containerid=1076036049220524'),
+        'options' => [
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+                'X-Requested-With' => 'XMLHttpRequest',
+                'Referer' => 'http://m.weibo.cn/u/6049220524',
+                'Host' => 'm.weibo.cn'
+            ],
+        ],
+        'download_path' => __DIR__ . '/downloads/weibo/6049220524'
+    ],
+    'yao' => [
+        'index' => new \yaochufa\YaochufaPipeline('GET', 'https://m.yaochufa.com/youji/list?event_time=1484056015&DeviceToken=E1076D4F-2A28-41D9-B58A-0DE05BAA073F&system=ios&appcitycode=440100&userId=0&version=5.5.4'),
+        'options' => [
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+            ],
+        ],
     ]
 ];
---
+++ example/weibo_hz/PagePipeline.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2016
+ *
+ * @link      https://www.github.com/janhuang
+ * @link      http://www.fast-d.cn/
+ */
+
+namespace weibo_hz;
+
+
+use GuzzleHttp\Exception\RequestException;
+use Pipeline\ImagePipeline;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class PagePipeline extends \Pipeline
+{
+    /**
+     * @param Crawler $crawler
+     * @param ResponseInterface $response
+     * @return mixed
+     */
+    public function process(Crawler $crawler = null, ResponseInterface $response = null)
+    {
+        $json = transformResponseToJson($response);
+
+        $total = 0;
+        $promises = [];
+        foreach ($json['cards'] as $card) {
+            if (isset($card['mblog']['pics'])) {
+                foreach ($card['mblog']['pics'] as $item) {
+                    $total++;
+                    $promises[] = $this->pipeline(new ImagePipeline('GET', $item['large']['url']));
+                }
+            }
+        }
+
+        progressBarMaxStep($total);
+        wait($promises);
+        unset($promises);
+    }
+
+    /**
+     * @param RequestException $exception
+     * @return mixed
+     */
+    public function error(RequestException $exception)
+    {
+        // TODO: Implement error() method.
+    }
+}
